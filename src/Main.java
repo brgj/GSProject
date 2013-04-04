5095b2474da702ce2f06fcbc00f28b65c0758fe0
brgj <brad.johnson155@gmail.com>
2013-04-04 13:20:34 -0700

    Stop bugging me, idiot

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: brad
+ * Date: 4/4/13
+ * Time: 1:19 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
